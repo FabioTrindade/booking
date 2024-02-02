604e0a4ed7970cdec3aae33090fcdd8cb9ced42d
FabioTrindade <fabio.trindade@hotmail.com>
2024-02-02 17:41:47 -0300

    feature(domain): simplifying the if return

--- Booking.Domain/Apartments/Money.cs
+++ Booking.Domain/Apartments/Money.cs
@@ -5,9 +5,7 @@ public record Money(decimal Amount, Currency Currency)
     public static Money operator +(Money first, Money second)
     {
         if(first.Currency != second.Currency)
-        {
             throw new InvalidOperationException("Currency have to be equal");
-        }
 
         return new Money(first.Amount + second.Amount, first.Currency);
     }
