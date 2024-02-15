7b1876e83041ac51a67cfc1eee0f5a830a275973
FabioTrindade <fabio.trindade@hotmail.com>
2024-02-15 20:47:28 -0300

    feat: added interfaces abstract cqrs and dependency injection

---
+++ Booking.Application/Abstractions/Messaging/ICommandHanlder.cs
@@ -0,0 +1,14 @@
+﻿using Booking.Domain.Abstractions;
+using MediatR;
+
+namespace Booking.Application.Abstractions.Messaging;
+
+public interface ICommandHanlder<TCommand> : IRequestHandler<TCommand, Result>
+    where TCommand : ICommand
+{
+}
+
+public interface ICommandHanlder<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+}
---
+++ Booking.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using Booking.Domain.Abstractions;
+using MediatR;
+
+namespace Booking.Application.Abstractions.Messaging;
+
+public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>
+{
+
+}
