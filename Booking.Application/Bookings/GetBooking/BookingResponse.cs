4bfafe9c12e99caf26e549e1638ed5221e29d3cb
FabioTrindade <fabio.trindade@hotmail.com>
2024-02-15 21:46:52 -0300

    feat: added response

---
+++ Booking.Application/Bookings/GetBooking/BookingResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Booking.Application.Bookings.GetBooking;
+
+public sealed class BookingResponse
+{
+    public Guid Id { get; init; }
+
+    public Guid UserId { get; init; }
+
+    public Guid ApartmentId { get; init; }
+}
---
+++ Booking.Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -0,0 +1,5 @@
+﻿using Booking.Application.Abstractions.Messaging;
+
+namespace Booking.Application.Bookings.GetBooking;
+
+public sealed record GetBookingQuery(Guid BookingId) : IQuery<BookingResponse>;
