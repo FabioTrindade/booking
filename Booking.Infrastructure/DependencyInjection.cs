4eb9772643c1bcc417523bcbcb74a0619fabec41
FabioTrindade <fabio.trindade@hotmail.com>
2024-03-19 18:57:27 -0300

    feat: added dependency injection

---
+++ Booking.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+using Booking.Application.Abstractions.Clock;
+using Booking.Application.Abstractions.Email;
+using Booking.Infrastructure.Clock;
+using Booking.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Booking.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+        services.AddTransient<IEmailService, EmailService>();
+
+        var connectionString =
+            configuration.GetConnectionString("Database") ??
+            throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+        });
+        
+        return services;
+    }
+}
