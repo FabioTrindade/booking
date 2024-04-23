a8610d32d1d2085128e0f64f55ccf278a59bcad9
FabioTrindade <fabio.trindade@hotmail.com>
2024-04-23 10:59:28 -0300

    feat: increase configuration entity

--- Booking.Infrastructure/Configurations/ReviewConfiguration.cs
+++ Booking.Infrastructure/Configurations/ReviewConfiguration.cs
@@ -1,13 +1,36 @@
+using Booking.Domain.Apartments;
+using Booking.Domain.Review;
 using Booking.Domain.Users;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Booking.Infrastructure.Configurations;
 
-public sealed class ReviewConfiguration : IEntityTypeConfiguration<User>
+public sealed class ReviewConfiguration : IEntityTypeConfiguration<Review>
 {
-    public void Configure(EntityTypeBuilder<User> builder)
+    public void Configure(EntityTypeBuilder<Review> builder)
     {
-        throw new NotImplementedException();
+        builder.ToTable("reviews");
+
+        builder.HasKey(review => review.Id);
+
+        builder.Property(review => review.Rating)
+            .HasConversion(rating => rating.Value, value => Rating.Create(value).Value);
+
+        builder.Property(review => review.Comment)
+            .HasMaxLength(200)
+            .HasConversion(commnet => commnet.Value, value => new Comment(value));
+
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(review => review.ApartmentId);
+        
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(review => review.BookingId);
+        
+        builder.HasOne<Apartment>()
+            .WithMany()
+            .HasForeignKey(review => review.UserId);
     }
 }
